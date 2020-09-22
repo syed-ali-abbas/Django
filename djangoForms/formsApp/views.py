90b78470a753622fbe301a74d5ccbd9e54c7f576
SYED ALI ABBAS <syedaliabbasshahmajeed@gmail.com>
2020-09-22 22:28:13 +0500

    CRUD operations in django blog Application by Syed Ali Abbas

--- djangoForms/formsApp/views.py
+++ djangoForms/formsApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 
 
 
@@ -14,8 +15,23 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+    
 
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogUpdate(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+
+class BlogDeletion(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+    
+    
